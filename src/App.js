bf8c33221523633d399cb0fe424311a0ea346f73
Matthew <mattheweng10@gmail.com>
2025-03-31 16:00:55 -0400

    Updated resume site files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './components/global.css';
+import Header from './components/Header';
+import RutgersLink from './components/RutgersLink';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import DarkModeToggle from './components/DarkModeToggle';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DarkModeToggle />
+      <Header />
+      <RutgersLink />
+      <main>
+        <About />
+        <Resume />
+        <Contact />
+      </main>
+      <Footer />
     </div>
   );
 }
